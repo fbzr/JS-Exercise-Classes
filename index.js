adfb9757c8cbab2edd3afd3b1de3f85869c17b70
fbzr <fabricio.spb@gmail.com>
2020-01-30 12:28:40 -0800

    TASK 2 - Car class

--- index.js
+++ index.js
@@ -77,7 +77,28 @@ class Person {
 */
 
 class Car {
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  
+  fill(gallons) {
+    this.tank += gallons;
+  }
 
+  drive(distance) {
+    const maxDistance = this.tank * this.milesPerGallon;
+
+    if (distance > maxDistance) {
+      this.odometer += maxDistance;
+      this.tank -= maxDistance/this.milesPerGallon;
+      return `I ran out of fuel at ${this.odometer} miles!`;
+    } 
+    this.odometer += distance;
+    this.tank -= distance/this.milesPerGallon;
+  }
 }
 
 /*
